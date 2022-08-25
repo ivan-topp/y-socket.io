a72115b9a628823222ad6dbde04883703fef1935
Iván Topp Sandoval <ivan.topp.sandoval@gmail.com>
2022-08-24 20:55:25 -0400

    [MOD] Improve react example

--- examples/react-example/src/App.tsx
+++ examples/react-example/src/App.tsx
@@ -3,50 +3,51 @@ import { useEffect, useState } from 'react';
 import { SocketIOProvider } from 'y-socket.io';
 
 function App() {
+  const [doc, setDoc] = useState<Y.Doc | null>(null);
   const [provider, setProvider] = useState<SocketIOProvider | null>(null);
   const [connected, setConnected] = useState<boolean>(false);
   const [input, setInput] = useState<string>('');
   const [clients, setClients] = useState<string[]>([]);
 
   useEffect(() => {
-    if (!provider) setProvider(new SocketIOProvider(
-      'ws://localhost:1234',
-      'testing-doc',
-      new Y.Doc(),
-      {
-        autoConnect: true,
-        // disableBc: true,
-        // auth: { token: 'valid-token' },
-        onConnect: () => {
-          setConnected(true);
-        },
-        onDisconnect: () => {
-          setConnected(false);
-        },
-      }));
-  }, [provider]);
-  
-  useEffect(() => {
-    if (!provider) return;
-    const yMap = provider.doc.getMap('data');
-    if (!yMap.has('input')) {
-      yMap.set('input', '');
-      yMap.observe((event, transaction) => {
-        setInput(yMap.get('input') as string);
-      });
+    if (!doc) {
+      console.log('setting doc')
+      const _doc = new Y.Doc();
+      const yMap = _doc.getMap('data');
+      if (!yMap.has('input')) {
+        yMap.set('input', '');
+        yMap.observe((event, transaction) => {
+          setInput(yMap.get('input') as string);
+        });
+      }
+      setDoc(_doc);
     }
-  }, [provider, input]);
+  }, [doc]);
 
   useEffect(() => {
-    if (!provider) return;
-    provider.awareness.on('update', () => {
-      setClients(Array.from(provider.awareness.getStates().keys()).map(key => `${key}`));
-    });
-    provider.awareness.setLocalState({
-      id: Math.random(),
-      name: 'Perico',
-    });
-  }, [provider]);
+    if (!!doc && !provider) {
+      console.log('setting providers')
+      const socketIOProvider = new SocketIOProvider(
+        'ws://localhost:1234',
+        'testing-doc',
+        doc,
+        {
+          autoConnect: true,
+          // disableBc: true,
+          // auth: { token: 'valid-token' },
+          onConnect: () => {
+            setConnected(true);
+          },
+          onDisconnect: () => {
+            setConnected(false);
+          },
+        });
+      socketIOProvider.awareness.on('change', () => setClients(Array.from(socketIOProvider.awareness.getStates().keys()).map(key => `${key}`)))
+      socketIOProvider.awareness.setLocalState({ id: Math.random(), name: 'Perico' });
+      socketIOProvider.on('sync', () => console.log('websocket sync'))
+      setProvider(socketIOProvider);
+    }
+  }, [doc, provider]);
 
   if (!provider) return <h1>Initializing provider...</h1>;
 
@@ -56,11 +57,11 @@ function App() {
       <div style={{ color: 'white' }}>
         <p>State: {connected ? 'Connected' : 'Disconneted'}</p>
         {
-          !provider.socket.connected
+          !connected
             ? <>
               <button onClick={() => provider.connect()}>Connect</button>
             </>
-            : <div style={{ display: 'flex', flexDirection: 'column' }}>
+            : !!doc &&<div style={{ display: 'flex', flexDirection: 'column' }}>
               <pre>
                 {
                   JSON.stringify(clients, null, 4)
@@ -68,10 +69,13 @@ function App() {
               </pre>
               <input
                 value={input}
-                onChange={(e) => provider.doc.getMap('data').set('input', e.target.value ?? '')}
+                onChange={(e) => {
+                  console.log('first')
+                  doc.getMap('data').set('input', e.target.value ?? '')
+                }}
               />
               <br />
-              <button onClick={() => provider.doc.getMap('data').set('input', `${Math.random()}`)}>Emit random change</button>
+              <button onClick={() => doc.getMap('data').set('input', `${Math.random()}`)}>Emit random change</button>
             </div>
         }
       </div>
